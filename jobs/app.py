9de4d74c228435cad7950b5acb493cbe5ec274c2
Connor <vanmeter.connor21@gmail.com>
2020-07-20 16:26:22 -0500

    Module 2 work

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,9 @@
-import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/jobs')
+def jobs(self):
+    return render_template('index.html')
