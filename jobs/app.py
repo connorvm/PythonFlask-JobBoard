cd27e225bb2271d5f73afd68dd2bd8b9ec113211
Connor <vanmeter.connor21@gmail.com>
2020-07-21 09:24:19 -0500

    Module4 work

--- jobs/app.py
+++ jobs/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def open_connection(self):
     connection = getattr(g, '_connection', None)
     if connection is None:
-        connection = g._connection, sqlite3.connect(PATH)
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
